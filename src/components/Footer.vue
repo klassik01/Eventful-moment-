f74ce6aa0b9ca8aabdaf2d4279988fc3f21dd5b6
Taiwo Sodiq <klassikklassik3@gmail.com>
2023-03-14 22:10:41 +0100

    eventful moment project

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="bg-footer flex justify-center text-sm items-end flex-col h-12 min-[100px]:px-4 md:px-16 absolute bottom-0 w-full mt-24">
-    <p>Eventful Moments by Hassan Warwick</p>
+    <p>Eventful Moments by Klassik</p>
   </footer>
 </template>
 
